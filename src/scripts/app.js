ec1b55c41715ca2431acf8be385a3da4339377f1
Jose Luis Barron <barronliconjose@gmail.com>
2016-08-17 08:07:15 -0500

    add layout and first behaviors

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,20 +5,45 @@
     }(function($, window, document) {
 
         /**
-         * @description On Document start
+         * @description On Document ready
          */
         $(function() {
-            getTime();
+            new Clock;
         });
-        getTime();
 
-        function getTime() {
-            let date = new Date;
-            let seconds = date.getSeconds();
-            let minutes = date.getMinutes();
-            let hours = date.getHours();
+        class Clock {
+            constructor() {
+                setInterval(() => this.updateTime(), 1000);
+            }
+
+            getHoursPosition() {
+                return Math.floor(0.5 * (60 * this.hours + this.minutes));
+            }
+
+            getMinutesPosition() {
+                return Math.floor(6 * this.minutes);
+            }
+
+            getSecondsPosition() {
+                return Math.floor(6 * this.seconds);
+            }
+
+            updateTime() {
+                this.date = new Date;
+                this.seconds = this.date.getSeconds();
+                this.minutes = this.date.getMinutes();
+                this.hours = ((this.date.getHours() + 11) % 12 + 1);
+
+                rotateElement('hours', this.getHoursPosition());
+                rotateElement('minutes', this.getMinutesPosition());
+                rotateElement('seconds', this.getSecondsPosition());
+            }
         }
 
+        function rotateElement(element, value) {
+            let trans = `rotate(${value}deg)`;
+            $(`.${element}`).css({ 'transform': trans, '-moz-transform': trans, '-o-transform': trans, '-webkit-transform': trans });
+        }
 
     })
 );
